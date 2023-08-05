e70344ec058fbb1335699c75849820f762f5fa6d
vincenzodivita <divitavincenzo@icloud.com>
2023-08-05 16:11:52 +0200

    Add switch case

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,50 @@ public class Main {
         // Ask user for a month
         String userMonth = scanner.nextLine().toLowerCase();
 
+        // Switch case
+        int daysNum;
+        switch (userMonth) {
+            case "gennaio":
+            daysNum = 31;
+            break;
+            case "febbraio":
+            daysNum = 31;
+            break;
+            case "marzo":
+            daysNum = 31;
+            break;
+            case "aprile":
+            daysNum = 31;
+            break;
+            case "maggio":
+            daysNum = 31;
+            break;
+            case "giugno":
+            daysNum = 31;
+            break;
+            case "luglio":
+            daysNum = 31;
+            break;
+            case "agosto":
+            daysNum = 31;
+            break;
+            case "settembre":
+            daysNum = 31;
+            break;
+            case "ottobre":
+            daysNum = 31;
+            break;
+            case "novembre":
+            daysNum = 31;
+            break;
+            case "dicembre":
+            daysNum = 31;
+            break;
+            default:
+                daysNum = 0;
+        }
+
         // Print test
-        System.out.println(userMonth);
+        System.out.println(userMonth + " " + daysNum);
     }
 }
